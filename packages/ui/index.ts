35b8672a86efaa1426546f280da46396cb173c8c
Fractal-Tess <vgfractal@gmail.com>
2024-11-23 08:42:50 +0200

    sync

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,1 +1,7 @@
 export * from './src/ui/button';
+export * from './src/ui/icon';
+// export * as Dialog from './src/ui/dialog';
+// export * as Label from './src/ui/label';
+// export * as Input from './src/ui/input';
+// export * as Select from './src/ui/select';
+// export * as Textarea from './src/ui/textarea';
---
+++ packages/ui/src/ui/icon/icon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+
+  export let icon: string;
+  let className: string = '';
+  export { className as class };
+</script>
+
+<Icon {icon} class={className} />
