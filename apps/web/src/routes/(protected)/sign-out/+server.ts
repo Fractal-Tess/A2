04b2d50bf8115cd143c89e21b79c642da3eaecc7
Fractal-Tess <vgfractal@gmail.com>
2024-11-24 04:18:40 +0200

    delete the session

--- apps/web/src/routes/(protected)/sign-out/+server.ts
+++ apps/web/src/routes/(protected)/sign-out/+server.ts
@@ -1,7 +1,11 @@
 import { dev } from '$app/environment';
 import { PUBLIC_SESSION_COOKIE } from '$env/static/public';
+import { createSessionClient } from '$lib/client/appwrite';
 
 export async function GET(event) {
+	const { account } = createSessionClient(event.cookies.get(PUBLIC_SESSION_COOKIE));
+	await account.deleteSession('current');
+
 	const headers = new Headers({
 		location: '/sign-in',
 		'set-cookie': event.cookies.serialize(PUBLIC_SESSION_COOKIE, '', {
